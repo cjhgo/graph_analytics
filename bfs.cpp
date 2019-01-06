0a5b618fedabdf2f30998f19de28ef7fed4f85a2
chen <1728533217en@gmail.com>
2019-01-07 00:36:40 +0800

    update bfs

--- bfs.cpp
+++ bfs.cpp
@@ -3,6 +3,7 @@
 #include<vector>
 #include<string>
 #include<map>
+#include<set>
 #include<queue>
 typedef char vertex_data_type;
 typedef int edge_data_type;
@@ -22,26 +23,38 @@ int main(int argc, char const *argv[])
     }
     std::queue<vid_type> q;
     q.push(0);
+    std::set<vid_type> inserted;
+    size_t cnt = 0;
     while( not q.empty())
     {
         vid_type  top = q.front();
         vertex_type v = g.get_vertex(top);        
         if( g.vertices[top] == '0')
+        {
+            ++cnt;
             std::cout<<v.invid<<"#"<<v.vid<<std::endl;
+        }            
         for(auto e : g.out_edges(v.vid))
         {
-            if( g.vertices[e.first] == '0')
+            if( g.vertices[e.first] == '0' and inserted.find(e.first) == inserted.end())
+            {
                 q.push(e.first);
+                inserted.insert(e.first);
+            }                
         }
         for(auto e : g.in_edges(v.vid))
         {
-            if( g.vertices[e.first] == '0')
+            if( g.vertices[e.first] == '0' and inserted.find(e.first) == inserted.end())
+            {
                 q.push(e.first);
+                inserted.insert(e.first);
+            }
         }    
         g.vertices[top]='1';
         q.pop();
+        std::cout<<"\n----"<<cnt*1.0/g.num_vertices()<<"----"<<std::endl;        
     }
-
+    std::cout<<cnt<<"\t"<<g.num_vertices()<<std::endl;
     
     return 0;
 }
