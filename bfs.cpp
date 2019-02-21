4d8f654eb69b397e32aef6d92be0185f388f02c9
chen <1728533217en@gmail.com>
2019-02-21 15:22:58 +0800

    bfs using colors

--- bfs.cpp
+++ bfs.cpp
@@ -6,7 +6,7 @@
 #include<map>
 #include<set>
 #include<queue>
-typedef char vertex_data_type;
+typedef enum{ WHITE,GRAY,BLACK } vertex_data_type;
 typedef int edge_data_type;
 typedef Graph<vertex_data_type, edge_data_type> graph_type;
 typedef typename graph_type::Vertex vertex_type;
@@ -23,46 +23,40 @@ int main(int argc, char const *argv[])
     }    
     g.finalized();
     std::cout<<"graph info:"<<std::endl<<g.num_vertices()<<"\t"<<g.num_edges()<<std::endl;
-    for(auto& e : g.vertices)
+    for(auto& ele : g.vertices)
     {
-        e='0';
+        ele=WHITE;
     }
     std::queue<vid_type> q;
-    if( argc >= 3)
-        q.push(std::atoi(argv[2]));
-    else
-        q.push(0);
-    std::set<vid_type> inserted;
+    size_t vid = 0;
+    if( argc >= 3) vid = std::atoi(argv[2]);
+    q.push(vid);
+    g.vertices[vid]=GRAY;
     size_t cnt = 0;
     while( not q.empty())
     {
         vid_type  top = q.front();
         vertex_type v = g.get_vertex(top);        
-        if( g.vertices[top] == '0')
-        {
-            ++cnt;
-            //std::cout<<v.invid<<"#"<<v.vid<<std::endl;
-            std::cout<<v.invid<<std::endl;
-        }            
+        std::cout<<v.invid<<std::endl;        
+        ++cnt;            
         for(auto e : g.out_edges(v.vid))
         {
-            if( g.vertices[e.first] == '0' and inserted.find(e.first) == inserted.end())
+            if( g.vertices[e.first] == WHITE)
             {
                 q.push(e.first);
-                inserted.insert(e.first);
+                g.vertices[e.first]=GRAY;
             }                
         }
         for(auto e : g.in_edges(v.vid))
         {
-            if( g.vertices[e.first] == '0' and inserted.find(e.first) == inserted.end())
+            if( g.vertices[e.first] == WHITE)
             {
                 q.push(e.first);
-                inserted.insert(e.first);
+                g.vertices[e.first]=GRAY;
             }
         }    
-        g.vertices[top]='1';
-        q.pop();
-        //std::cout<<"\n----"<<cnt*1.0/g.num_vertices()<<"----"<<std::endl;        
+        g.vertices[top]=BLACK;
+        q.pop();     
     }
     std::cout<<cnt<<"\t"<<g.num_vertices()<<std::endl;
     
